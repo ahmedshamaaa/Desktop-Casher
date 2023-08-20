c5bc9fc4415c95f93eb5b9fc4b786dda7346c041
“[ahmedshamaaa]” <[ahmedshamaa135@gmail.com]>
2023-08-20 23:56:28 +0300

    “after_update”

--- test/src/application/OnInvoiceFinalizedListener.java
+++
@@ -1,4 +0,0 @@
-package application;
-public interface OnInvoiceFinalizedListener {
-    void onInvoiceFinalized();
-}
--- test/src/module-info.java
+++
@@ -1,7 +0,0 @@
-module test {
-	requires javafx.controls;
-	requires javafx.fxml;
-	requires java.sql;
-	
-	opens application to javafx.graphics, javafx.fxml;
-}
